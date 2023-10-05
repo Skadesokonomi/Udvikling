6d98bc7d1aa5e3e790e199db3d3ce3acf9ee7be0
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2023-10-05 21:00:17 +0200

    Ny ikon

--- eco_model/pyrcc5x.cmd
+++ eco_model/pyrcc5x.cmd
@@ -1,7 +1,9 @@
 @echo on
-set qprogdir="C:\Program Files\QGIS 3.20.0\bin"
+set qprogdir="C:\Program Files\QGIS 3.32.1\bin"
 call %qprogdir%\o4w_env.bat
 call %qprogdir%\qt5_env.bat
 call %qprogdir%\py3_env.bat
 @echo on
 pyrcc5 -o "%0\..\resources.py" "%0\..\resources.qrc"
+pause
+
